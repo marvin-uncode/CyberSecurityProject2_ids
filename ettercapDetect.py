07140177917bf646766dbe489da695e66ba9b0c0
Alex Lewin <asl0028@auburn.edu>
2019-12-03 11:57:15 -0500

    Minor Adjust

--- ettercapDetect.py
+++ ettercapDetect.py
@@ -1,6 +1,7 @@
 import pyshark
-capture = pyshark.LiveCapture(interface='eth0', display_filter='arp.duplicate-address-frame')
+capture = pyshark.LiveCapture(interface='eth0')
 for packet in capture.sniff_continuously():
+  if packet.arp.duplicate-address-frame:
     print("ARP CACHE POISONING DETECTED")
     print("Attacker Machine:", str(packet.ip.src))
 capture.sniff()
