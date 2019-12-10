2fef8ec6d3dcad5b325d86948ab495c83a2b5107
Alex Lewin <asl0028@auburn.edu>
2019-12-10 15:55:51 -0600

    Finished, added Ack scan detect

--- nmapDetect.py
+++ nmapDetect.py
@@ -1,5 +1,9 @@
 import pyshark
+from collections import defaultdict
 capture = pyshark.LiveCapture(interface='eth0')
+
+ack_ports = defaultdict(set()) #src port -> dst port 
+
 for packet in capture.sniff_continuously():
   if tcp && tcp.flags.fin == 1 
     && (tcp.window_size==1024 || 
@@ -10,7 +14,14 @@ for packet in capture.sniff_continuously():
     print("Attacker Machine:", str(packet.ip.src))
 
 
-  if tcp && tcp.flags==0x29 && tcp.flags.fin==1 && tcp.flags.push==1 && tcp.flags.urg==1:
+  elif tcp && tcp.flags==0x29 && tcp.flags.fin==1 && tcp.flags.push==1 && tcp.flags.urg==1:
     print('NMAP XMAS SCAN DETECTED')
     print("Attacker Machine:", str(packet.ip.src))
+  
+  elif tcp && tcp.flags==0x10 && tcp.flags.ack==1:
+    ack_ports[tcp.srcport].add(tcp.dstport)
+    if len(ack_ports[tcp.srcport]) > 15:
+      print('NMAP ACK SCAN DETECTED')
+      print("Attacker Machine:", str(packet.ip.src))
+   
 capture.sniff()
